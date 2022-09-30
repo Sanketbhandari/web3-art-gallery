d69292a96a3651defca7650e706a01d8f0d9b060
Sanketbhandari <sanketbhandari22@gmail.com>
2022-09-30 20:09:31 +0530

    added icon state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ethers } from "ethers";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [account, setAccount] = useState("");
+
+  const initConnection = async () => {
+    if (typeof window.ethereum !== "undefined") {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setAccount(accounts[0]);
+    } else {
+      console.log("Please install MetaMask");
+    }
+  };
+
+  useEffect(() => {
+    initConnection();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <div className="header">
+        <img className="artIcon" src={require(`./assets/images/s-logo.png`)} />
+        <p>11/15</p>
+        {account == "" ? (
+          <button className="button" onClick={initConnection}>
+            Connect
+          </button>
+        ) : (
+          <p>{account}</p>
+        )}
+      </div>
     </div>
   );
 }
